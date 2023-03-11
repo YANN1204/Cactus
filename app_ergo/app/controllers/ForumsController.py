9b3406a1b8c06befe8a62ccb9e011c0e08f45ef4
Léa Vernay <lea@gibraltaz.fr>
2023-03-11 21:38:03 +0100

    liste forums v1

--- app_ergo/app/controllers/ForumsController.py
+++ app_ergo/app/controllers/ForumsController.py
@@ -6,10 +6,10 @@ from app.services.servicesGETData import GetDataServices
 gds = GetDataServices()
 
 basepath = '/'
-url = "alternative_cards"
+url = "forums"
 
 @app.route(basepath + 'forums', methods = ['GET'])
 def forums():
-    data = gds.alternative_by_title(url, "Boire l'eau du robinet grâce à une carafe filtrante")
+    data = gds.display_data(url)
     metadata = {"title":"Forums", "pagename": "forums"}
     return render_template('forums.html', data=data, metadata=metadata)
