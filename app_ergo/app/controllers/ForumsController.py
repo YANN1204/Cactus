333ed94e02e3032f073ec1ff501cd61cb61a81c5
Léa Vernay <lea@gibraltaz.fr>
2023-03-13 16:39:07 +0100

    ajout forum.html et modif pour ouvrir 1 seul forum

--- app_ergo/app/controllers/ForumsController.py
+++ app_ergo/app/controllers/ForumsController.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import Flask, render_template, request
 from app import app
 
 from app.services.servicesGETData import GetDataServices
@@ -12,9 +12,17 @@ urlt = "tags"
 urlft = "forums_tags"
 urlr = "rooms"
 
+
 @app.route(basepath + 'forums', methods = ['GET'])
 def forums():
     data = gds.display_forums_fiches(urlf, urlu, urlft, urlt, urlr)
     metadata = {"title":"Forums", "pagename": "forums"}
     return render_template('forums.html', data=data, metadata=metadata)
 
+
+@app.route('/forum')
+def forum():
+    idForum = request.args.get('idForum', None)
+    data = gds.item_by_id(urlf, idForum)
+    metadata = {"title":"Forum", "pagename": "forum"}
+    return render_template('forum.html', data = data, metadata=metadata)
