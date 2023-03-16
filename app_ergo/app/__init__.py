90378741184b40fdeea927c73afc1c27c1b1db38
YANN1204 <yann.riopro@gmail.com>
2023-03-16 17:04:57 +0100

    ajout fonctionnalité de connexion pour n'importe quel utilisateur

--- app_ergo/app/__init__.py
+++ app_ergo/app/__init__.py
@@ -1,5 +1,22 @@
-from flask import Flask
+import signal
+from flask import Flask, session
+from flask_session import Session
+import sys
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.secret_key = 'une_cle_secrete_pour_la_session'
+Session(app)
+
+def signal_handler(signal, frame):
+    # Réinitialiser la session Flask lorsque l'application est arrêtée
+    session.clear()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
 
 from app.controllers import *
